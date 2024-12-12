a2065312469c43bd3e9a13d60c8d75caf34eed2e
DesRob211 <drobinson211@gmail.com>
2024-12-12 11:44:05 -0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.header('New and Used Vehicles')
-st.write(""" #### Manipulate the data below to compare different filters.""")
-the_new = st.checkbox('Include new cars')
+st.write(""" #### Comparing New and Used cars.In the data below to compare based on different factors.""")
+st.checkbox('Include new cars')
+
+data = pd.read_csv('vehicles_us.csv')
+cars = data[data['type'].isin(['sedan', 'coupe','convertible', 'hatchback'])]
+family = data[data['type'].isin(['SUV', 'wagon', 'van', 'mini-van'])]
+trucks = data[data['type'].isin(['pickup', 'truck', 'offroad'])]
+party = data[data['type'].isin(['other','bus'])]
+
+fig = px.histogram(data,
+                   x='condition',
+                   y='model_year',
+                   histfunc='count',
+                   title='Car Condition vs. Model Year')
+
+event = st.plotly_chart(fig)
+
---
+++ test.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.write("""
+  # Hello World!!
+  # this is my **first**  *app*!
+  #        
+""")
