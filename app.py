9d05c0044e61cd4d00aabe4d28faf0f220bd26ae
DesRob211 <drobinson211@gmail.com>
2024-12-16 13:08:25 -0800

    big change

--- app.py
+++ app.py
@@ -7,16 +7,24 @@ st.write(""" #### Comparing New and Used cars.In the data below to compare based
 st.checkbox('Include new cars')
 
 data = pd.read_csv('vehicles_us.csv')
-cars = data[data['type'].isin(['sedan', 'coupe','convertible', 'hatchback'])]
-family = data[data['type'].isin(['SUV', 'wagon', 'van', 'mini-van'])]
-trucks = data[data['type'].isin(['pickup', 'truck', 'offroad'])]
-party = data[data['type'].isin(['other','bus'])]
 
-fig = px.histogram(data,
-                   x='condition',
-                   y='model_year',
-                   histfunc='count',
-                   title='Car Condition vs. Model Year')
+data
 
-event = st.plotly_chart(fig)
+car_condition = data['type'].unique()
+
+data_type = st.selectbox('Pick your vehicle type', car_condition)
+min_year, max_year = int(data['model_year'].min()), int(data['model_year'].max())
+
+
+years_invovled = st.slider('Which years did you need?', value=(min_year, max_year), min_value=min_year, max_value=max_year)
+years_invovled[0]
+years_invovled[1]
+
+real_range = list(range(years_invovled[0], years_invovled[1]+1))
+
+df_select = data[(data['type'] == data_type) & (data['model_year'].isin(list(real_range)))]
+
+df_select
+
+
 
